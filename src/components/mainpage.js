b48e65a7c8dfe69a319700b9d76d24485d365f5f
Samdeep Sharma <121767920+SamdeepSharma@users.noreply.github.com>
2023-09-22 01:16:43 +0530

    Made API functional

--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -16,27 +16,36 @@ const Mainpage = () => {
      const handleData = async () => {
           setLoadingmsg('Fetching stock info...');
           try {
-               const api_key = 'ck5kpo9r01qls0um8t60ck5kpo9r01qls0um8t6g';
-               const response = await fetch(`https://finnhub.io/api/v1/quote?symbol=${stockcode}&token=${api_key}`);
+               const api_key1 = 'ck677l1r01qqsl3265v0ck677l1r01qqsl3265vg';
+               const response = await fetch(`https://finnhub.io/api/v1/quote?symbol=${stockcode}&token=${api_key1}`);
 
                if (response.ok) {
                     setLoadingmsg('');
                     const data = await response.json();
                     setFetchData(data);
                     setSymbol(stockcode);
-               
-                    if (fetchData.c == 0){
-                         setLoadingmsg('Please enter a VALID STOCK CODE!!');
-                         setSymbol(null);}
                }
+               else {
+                    const api_key2 = 'ck5kpo9r01qls0um8t60ck5kpo9r01qls0um8t6g';
+                    const nresponse = await fetch(`https://finnhub.io/api/v1/quote?symbol=${stockcode}&token=${api_key2}`);
+                    
+                    if (nresponse.ok) {
+                         setLoadingmsg('');
+                         const data = await nresponse.json();
+                         setFetchData(data);
+                         setSymbol(stockcode);
+                         
+                    }
+               }
+               
           }
           catch (error) {
-               setLoadingmsg('No such Stock Code exists!!');
+               setLoadingmsg('Page is temporarily down :(');
                setFetchData(null);
-               console.log('error');
+               console.log(error);
           }
      }
-     
+
      return (
           <div className='main'>
                <h1>Stock Tracking Redefined</h1>
@@ -48,19 +57,31 @@ const Mainpage = () => {
 
                <h6>{loadingmsg}</h6>
                {
-               fetchData && symbol &&
+                    fetchData && symbol && fetchData.d &&
                     <div className="fetchedData">
                          <p>Stock code: {symbol}</p>
                          <p>Stock Price: {fetchData.c} <span className='${symbol}'> USD</span></p>
                          <p>Price change: {fetchData.d}<span> USD</span></p>
                          <p>Percent price change: {fetchData.dp}<span>%</span></p>
-                         <p>Opened at: {fetchData.o}<span>USD</span> </p>
+                         <p>Opened at: {fetchData.o}<span> USD</span> </p>
                          <p>Previous close: {fetchData.pc}<span> USD</span></p>
                          <p>Today's Range: {fetchData.l} - {fetchData.h}</p>
                          <p>Timestamp: {fetchData.t}</p>
 
                     </div>
                }
+               {
+                    (!symbol) && fetchData &&
+                    <div className="fetchedData">
+                         <p>Please enter a STOCK CODE!!</p>
+                    </div>
+               }
+               {/* {
+                    (fetchData.c == "0") && symbol && fetchData &&
+                    <div className="fetchedData">
+                         <p>Please enter a VALID STOCK CODE!!</p>
+                    </div>
+               } */}
           </div>
      );
 }
